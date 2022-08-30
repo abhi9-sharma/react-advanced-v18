db0bf915d9581c3c60858548fe8c361eef2a63c4
Abhinav Sharma <abhinav19102@iiitnr.edu.in>
2022-08-30 20:52:21 +0530

    useState simple use case

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+
+const handleClick = () =>{
+  let title = 'new title';
+  console.log(title);
+};
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = 'random title'
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+      click here
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
