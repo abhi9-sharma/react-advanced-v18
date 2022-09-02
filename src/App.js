961e3719cdaae739dd2153e1a58f1d658d42e2b7
Abhinav Sharma <abhinav19102@iiitnr.edu.in>
2022-09-02 15:20:48 +0530

    4-forms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
+import React from 'react';
+import Setup from './tutorial/4-forms/setup/2-multiple-inputs';
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <Setup></Setup>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,59 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [people, newPeople] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && email) {
+      const person = { firstName, email };
+      newPeople((people) => {
+        return [...people, person];
+      });
+      setFirstName('');
+      setEmail('');
+      // console.log(people);
+    } else console.log('empty values');
+  };
+  return (
+    <>
+      <article>
+        <form action="" className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name: </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">E-mail: </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type="submit">add person</button>
+        </form>
+      </article>
+      {people.map((x, index) => {
+        const { firstName, email } = x;
+        return (
+          <h6 key={index}>
+            {firstName} {email}
+          </h6>
+        );
+      })}
+    </>
+  );
 };
 
 export default ControlledInputs;
